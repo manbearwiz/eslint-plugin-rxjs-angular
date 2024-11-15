8dc3c4e38406aeced8d5ad93a4d88d6618e89a3f
Kevin Brey <manbearwiz@gmail.com>
2024-11-15 01:50:59 -0600

    fix(index): add metadata

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import type { Linter } from '@typescript-eslint/utils/ts-eslint';
+import fs from 'node:fs';
 
 import rules from './rules';
 
+const pkg = JSON.parse(fs.readFileSync(new URL('../package.json'), 'utf8'));
+
 export = {
+  meta: {
+    name: pkg.name,
+    version: pkg.version,
+  },
   rules,
 } satisfies Linter.Plugin;
