b85dcac2e6143435ca1997ef5d8c299d372580fe
Kevin Brey <manbearwiz@gmail.com>
2024-11-15 10:11:26 -0600

    fix(index): get metadata from package.json

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
 import type { Linter } from '@typescript-eslint/utils/ts-eslint';
-import fs from 'node:fs';
+const { name, version } = require('../package.json');
 
 import rules from './rules';
 
-const pkg = JSON.parse(fs.readFileSync(new URL('../package.json'), 'utf8'));
-
 export = {
   meta: {
-    name: pkg.name,
-    version: pkg.version,
+    name,
+    version,
   },
   rules,
 } satisfies Linter.Plugin;
